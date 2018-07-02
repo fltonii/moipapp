8ef0c38b47b2081544ed78455604ca9b24bd151e
Joao Tonial <joao.tonial@beehiveclub.com.br>
2018-07-02 14:51:11 -0300

    MoIP exaple working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :orders, only: [:new, :create]
+  root to: 'orders#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
