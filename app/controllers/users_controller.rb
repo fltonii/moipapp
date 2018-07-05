e509b24a10a79ce4174f0f0f98466985ffe2d3dd
Joao Tonial <joao.tonial@beehiveclub.com.br>
2018-07-05 18:18:43 -0300

    formating users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,27 +3,47 @@ class UsersController < ApplicationController
   end
 
   def create
-  	api = Moip.new.call
-    uniqueId = SecureRandom.hex
-    customer = api.customer.create(
-    		ownId: uniqueId,
-    		email: params[:user][:email],
-    		fullname: params[:user][:fullname],
-    		birthDate: params[:user][:birthDate],
-    		taxDocument: {
-              type: "CPF",
-              number: params[:user][:cpf],
-          },
-  	    phone: {
-  	      countryCode: "55",
-  	      areaCode: params[:user][:areaCode],
-  	      number: params[:user][:phoneNumber],
-  	    },
+  	@api = Moip.new.call
+    @uniqueId = SecureRandom.hex
+    customer = @api.customer.create(user_json)
+    @moipid = customer.id
 
-      )
-      @moipid = customer.id
+    card = @api.customer.add_credit_card(@moipid, card_json)
+    @ccid = card.credit_card.id
 
-    card = api.customer.add_credit_card(@moipid, {
+    User.create(
+      fullname: params[:user][:fullname],
+      email: params[:user][:email],
+      ownid: @uniqueId,
+      phoneArea: params[:user][:areaCode],
+      cpf: params[:user][:cpf],
+      phoneNumber: params[:user][:phoneNumber],
+      moipid: @moipid,
+    )
+  end
+
+  private
+
+  def user_json
+  {
+    ownId: @uniqueId,
+      email: params[:user][:email],
+      fullname: params[:user][:fullname],
+      birthDate: params[:user][:birthDate],
+      taxDocument: {
+            type: "CPF",
+            number: params[:user][:cpf],
+        },
+      phone: {
+        countryCode: "55",
+        areaCode: params[:user][:areaCode],
+        number: params[:user][:phoneNumber],
+      },
+    }
+  end
+
+  def card_json
+    {
        method: "CREDIT_CARD",
        creditCard: {
          expirationMonth: params[:cc][:expirationMonth],
@@ -44,17 +64,6 @@ class UsersController < ApplicationController
            },
          },
        },
-     })
-     @ccid = card.credit_card.id
-
-     User.create(
-       fullname: params[:user][:fullname],
-       email: params[:user][:email],
-       ownid: uniqueId,
-       phoneArea: params[:user][:areaCode],
-       cpf: params[:user][:cpf],
-       phoneNumber: params[:user][:phoneNumber],
-       moipid: @moipid,
-     )
-  end
+     }
+   end
 end
